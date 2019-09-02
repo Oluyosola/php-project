9842c692aba9d106af4fc99403322c346aaa1d7c
0luyosola <afolabioluyosola@gmail.com>
2019-09-02 14:06:38 +0100

    updated my php project

--- index.php
+++ index.php
@@ -4,14 +4,19 @@ $b = 4500;
 $accumulated_a = $a + 1200;
 $accumulated_b = $b + 1500;
 echo "accumulated salary of worker a is ".$accumulated_a;
-echo "accumulated salary of worker b is" .$accumulated_b;
+echo "<br>";
+echo "accumulated salary of worker b is " .$accumulated_b;
+echo "<br>";
 $work_days_a = 30;
 $work_days_b = 28;
-echo "amount earned by worker a is".$accumulated_a/$work_days_a;
-echo "amount earned by worker b is".$accumulated_b/$work_days_b;
+echo "amount earned by worker a is ".$accumulated_a/$work_days_a;
+echo "<br>";
+echo "amount earned by worker b is ".$accumulated_b/$work_days_b;
 $work_days= 5;
-echo "amount earned in five days by a".$accumulated_a/$work_days;
-echo "amount earned in five days by b".$accumulated_b/$work_days;
+echo "<br>";
+echo "amount earned in five days by a ".$accumulated_a/$work_days;
+echo "<br>";
+echo "amount earned in five days by b ".$accumulated_b/$work_days;
 
 
 ?> 
