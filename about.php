a1132e05f5182e73a6d105bd9384f605a29214e2
0luyosola <afolabioluyosola@gmail.com>
2019-09-03 09:34:45 +0100

    updated my php project

--- about.php
+++ about.php
@@ -8,6 +8,6 @@ define ("pi",3.147);
 $radius = 5;
 $area_of_a_circle = pi*($radius^2);
 echo "the area of a circle is ". $area_of_a_circle;
-
-
+statement = true;
+echo !statement;
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
+//This program calculates the daily salary and salary earned in five days by two workers//
 define('base_salary', 4500);
 $accumulated_a = base_salary+ 1200;
 $accumulated_b = base_salary+ 1500;
+//outputs accumulated salary of worker a and b//
 echo "accumulated salary of worker a is ".$accumulated_a;
 echo "accumulated salary of worker b is " .$accumulated_b;
 $work_days_a = 30;
 $work_days_b = 28;
 $one_day_salary_of_a =$accumulated_a/$work_days_a;
 $one_day_salary_of_b =$accumulated_a/$work_days_b;
+//outputSs the daily salary of worker a and b//
 echo "amount earned by worker a is ". $one_day_salary_of_a;
 
 echo "amount earned by worker b is ".$one_day_salary_of_b;
 $work_days= 5;
+//outputs the salary earned by worker a and worker b in five days//
 echo "amount earned in five days by a ".$one_day_salary_of_a * $work_days;
 echo "amount earned in five days by b ".$one_day_salary_of_b* $work_days;
 
