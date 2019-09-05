b30fba3d1b3353eb1563dab48d31269f42b77903
0luyosola <afolabioluyosola@gmail.com>
2019-09-05 10:35:07 +0100

    functions

--- functions.php
+++ functions.php
@@ -1,23 +1,19 @@
 <?php
 // calculates simple interest
-function simpleInterest(float $principal, float $rate,  float $time) {
-    
-    static $rate = 3.5;
+function simpleInterest($principal, $time) {
+    $rate = 3.5;
+
  $simple_interest = (($principal* $rate *$time)/100);
     return  $simple_interest;
-    
-echo  "simple interest is $principal. $rate. $time";
 
 }
 
-echo  "simple interest is" . simpleInterest(2000.00, 3.5, 60.00);
 
 // prints the product of two variables
 
-function calculate(int $x, int $y){
+function calculate($x,  $y){
 $z = $x * $y;
 return $z;
 
 } 
-echo calculate (5,8);
 ?>
