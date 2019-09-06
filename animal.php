1a8a2da21b516f51ea149bf1df9ba61a328c9209
0luyosola <afolabioluyosola@gmail.com>
2019-09-06 11:55:45 +0100

    animals

--- animal.php
+++ animal.php
@@ -1,8 +1,28 @@
 <?php
-function animalName(){
-    return array ( '65kg', '72kg', '40kg');
+
+function animalName($animals) {
+
+if ($animals == "cat"){
+    $weight = 65;
+}  
+elseif ($animals == "dog"){
+    $weight = 72;
+   
+
+} elseif ($animals == "sheep"){
+    $weight = 40;
+
+}else {
+    $weight = 0;
 }
-list($cat, $dog, $sheep) = animalName();
- 
-min (animalName());
+return $weight;
+}
+function getWeight($animals){
+    return getWeight(); 
+}
+$animals  = array("cat" => 65, "dog" => 72, "aheep" => 40);
+sort($animals);
+
+print_r($animals);
+
 ?>
