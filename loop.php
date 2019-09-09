4684eca1013b51d2f6900ad6d0af93b0e27e5682
0luyosola <afolabioluyosola@gmail.com>
2019-09-09 13:34:09 +0100

    update

--- loop.php
+++ loop.php
@@ -1,12 +1,22 @@
 <?php
+
+	
 $first_number = 1;
 $last_number = 10;
-
-$sum = 0;
-for ($i = $first_number; $i <= $last_number; $i++) {
-    $sum += $i;
-}
-
-echo "Sum of the first ten numbers" . " = " . $sum;
-
+	
+$sum=0;
+	
+while($first_number <= $last_number)
+	 
+{
+	  
+$sum=$sum + $first_number;
+	  
+$first_number++;
+	  
+}	
+	
+echo "Sum of the first ten numbers" . " = " . $sum;	
+  
 ?>
+
