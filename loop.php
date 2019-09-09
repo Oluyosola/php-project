2bb7a83fe48b06579d2c69b679b07c1426d5a998
0luyosola <afolabioluyosola@gmail.com>
2019-09-09 16:17:00 +0100

    update

--- loop.php
+++ loop.php
@@ -1,6 +1,5 @@
 <?php
-
-	
+// this code prints the sum of the first ten numbers 
 $first_number = 1;
 $last_number = 10;
 	
@@ -10,7 +9,7 @@ while($first_number <= $last_number)
 	 
 {
 	  
-$sum=$sum + $first_number;
+$sum= $sum + $first_number;
 	  
 $first_number++;
 	  
