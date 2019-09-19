dac873d2050775522d6dd7c8c409ff18361c5079
0luyosola <afolabioluyosola@gmail.com>
2019-09-19 17:26:33 +0100

    update

--- cal.php
+++ cal.php
@@ -1,27 +1,26 @@
 <?php
-//it does simple calculation using form
+//it does simple calculation using form 
 if(isset($_GET['calculator'])):
     $a_numeric = $_GET['a'];
     $b_numeric = $_GET['b'];
-    $operation = $_GET['Operations'];
-    if(is_numeric($a_numeric) && ($b_numeric )){
-        echo "it is numeric";
+    $operation = $_GET['operations'];
+    if(is_numeric($a_numeric) && is_numeric($b_numeric )){
+        // echo "it is numeric";
+        if ($operation == "add"){
+            $calculate = $a_numeric +  $b_numeric;
+        }elseif ($operation == "subtract"){
+            $calculate = $a_numeric - $b_numeric;
+        }
+        elseif($operation == "multiply"){
+            $calculate = $a_numeric *  $b_numeric;
+        }elseif($operation == "divide"){
+            $calculate = $a_numeric / $b_numeric;
+      }
+      else{
+     $calculate = "please select an operation";
+        }
+        echo $calculate;
     }else{
-        echo "it is not numeric";
+        echo "either A or B is not numeric";
     }
 endif;
-if ($operation == "add"){
-        $calculate = $a_numeric +  $b_numeric;
-    }elseif ($operation == "subtract"){
-        $calculate = $a_numeric - $b_numeric;
-    }
-    elseif($operation == "multiply"){
-        $calculate = $a_numeric *  $b_numeric;
-    }elseif($operation == "divide"){
-        $calculate = $a_numeric / $b_numeric;
-  }
-  else{
- echo "invalid action";
-    }
-    echo $calculate;
-?>
--- formcal.php
+++ formcal.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>attendance form</title>
+        <title>CALCULATOR</title>
     </head>
     <body>
 <h2>CALCULATOR</h2>
@@ -13,13 +13,15 @@
 <input type="text" name="a"> <br> B:
 <input type="text" name="b"> <br>
 Operations:
-<select name="Operations">
-    <option value=""></option>
+<select name="operations"> 
+    <option value="">please select</option>
     <option value ="add">Add</option>
     <option value ="subtract">Subtract</option>
     <option value ="multiply">Multiply</option>
     <option value ="division">Division</option>
  </select>
-    <input type="hidden" name="calculator" value="1" />
-    <p><input type="submit" value="Calculate"></p>
+    <!-- <input type="hidden" name="calculator" value="1" /> -->
+    <p><input type="submit" value="Calculate" name="calculator"></p>
 </form>
+</body>
+</html>
